59293729677fdb5f654da8ecdcf47b360b3b646d
Josh Pittman <code@joshpittman.com>
2023-03-08 14:20:20 -0500

    cleaned round type

--- src/models/Round.ts
+++ src/models/Round.ts
@@ -1,17 +1,5 @@
 import Card from "@classes/Card";
 import Hand from "@models/Hand";
-enum RoundState {
-  "NEW_ROUND",
-  "DEAL_POCKET",
-  "DEAL_FLOP",
-  "DEAL_TURN",
-  "DEAL_RIVER",
-  "END_ROUND",
-}
-
-type RoundAction = {
-  type: RoundState;
-};
 
 export type Round = {
   community: Card[];
